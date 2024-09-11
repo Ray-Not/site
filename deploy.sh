a59151b614a74c4b14402a22039c90a2b7a46392
root <root@ivxrhcuilk.local>
2024-09-11 13:37:32 +0000

    Add deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Переход в рабочую директорию проекта
+cd /root/site || exit
+
+# Обновление репозитория
+git fetch origin
+git reset --hard origin/main
+
+# Установка зависимостей
+pip install -r requirements.txt
+
+# Выполнение миграций и других задач
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+# Перезагрузка сервера
+systemctl restart gunicorn  # Замените на вашу службу, например, gunicorn или uwsgi
