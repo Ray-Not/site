03464ca30642b2f8ab9d77ee0e2aa7a995198434
Ray-Not <2sinsincuba@gmail.com>
2024-09-19 23:46:14 +0500

    Add marked catalogs

--- web/context_processors.py
+++ web/context_processors.py
@@ -5,10 +5,13 @@ from django.db.models import Count
 def catalog_context(request):
     chapters_with_titles = Catalog.objects.values(
         'chapter'
-    ).annotate(titles=Count('title'))
-    catalogs_btn = Catalog.objects.all().order_by('chapter', 'title')
+    ).annotate(
+        count=Count('title'),
+    ).order_by('chapter')
+
+    catalogs = Catalog.objects.all().order_by('chapter', 'title')
 
     return {
         'chapters_with_titles': chapters_with_titles,
-        'catalogs_btn': catalogs_btn,
+        'catalogs': catalogs,
     }
