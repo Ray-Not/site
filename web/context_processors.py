ab1dd5a173239b8848f5f53a704af748130205f0
Ray-Not <2sinsincuba@gmail.com>
2024-09-20 01:56:49 +0500

    Add header handler for mobile

--- web/context_processors.py
+++ web/context_processors.py
@@ -1,10 +1,11 @@
 from django.db.models import Count
-from .models import Catalog, Door
+from .models import Catalog
+
 
 def catalog_context(request):
     # Получаем каталоги и подсчитываем количество дверей для каждого каталога
     catalogs_with_door_count = Catalog.objects.annotate(
-        door_count=Count('catalogs')  # Подсчет количества дверей для каждого каталога
+        door_count=Count('catalogs')  # Подсчет количества дверей
     ).order_by('chapter', 'title')
 
     # Группируем каталоги по главам
