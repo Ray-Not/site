64efd936f5e42ba6432347cbfa9d87860dc0ff78
FairuzovR <skyfairrr@gmail.com>
2024-10-08 04:27:43 +0500

    Fix bugs

--- web/templatetags/custom_filters.py
+++ web/templatetags/custom_filters.py
@@ -14,9 +14,9 @@ def split_and_get_one(value):
 
     php_images = [url for url in urls if url.endswith('.php')]
     if php_images:
-        return php_images[:1]
+        return php_images[0]  # Возвращаем только первое изображение
 
-    return [urls[0]]
+    return urls[0] if urls else ''
 
 
 @register.filter
