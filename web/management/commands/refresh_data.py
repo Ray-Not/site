a145c79a60175b93f4357d2fce3769ded204b84e
FairuzovR <skyfairrr@gmail.com>
2024-10-10 20:12:31 +0500

    Fix nugs

---
+++ web/management/commands/refresh_data.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from web.models import Door
+import random
+
+used_numbers = set()
+
+
+def generate_unique_number(min_value=1, max_value=22):
+    while True:
+        number = random.randint(min_value, max_value)
+        if number not in used_numbers:
+            used_numbers.add(number)
+            return number
+
+
+class Command(BaseCommand):
+    help = 'Меняет название дверям'
+
+    def handle(self, *args, **kwargs):
+        doors = Door.objects.filter(title__icontains="Bravo")
+
+        count = doors.count()
+        self.stdout.write(self.style.SUCCESS(f'Найдено {count} дверей.'))
+
+        for door in doors:
+            random_number = generate_unique_number()
+            new_title = f"Входная металлическая дверь в квартиру Акрон {random_number}"
+            door.title = new_title
+            door.save()
+            self.stdout.write(self.style.SUCCESS(f'Обновлено: {new_title}'))
