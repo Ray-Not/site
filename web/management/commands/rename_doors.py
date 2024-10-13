16372c82392c2eb860dd046e825007ec1d571c20
FairuzovR <skyfairrr@gmail.com>
2024-10-13 18:14:15 +0500

    Fix bugs

--- web/management/commands/rename_doors.py
+++ web/management/commands/rename_doors.py
@@ -1,6 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.db import IntegrityError
-
 from web.models import Door
 from slugify import slugify
 
@@ -8,14 +6,26 @@ from slugify import slugify
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-
         doors = Door.objects.all()
 
         for door in doors:
-            try:
-                door.slug = slugify(door.title)
-            except IntegrityError:
-                door.slug = f"{slugify(door.title)}" + "P"
+            base_slug = slugify(door.title)
+            unique_slug = self.get_unique_slug(base_slug)
+
+            door.slug = unique_slug
             door.description = f'Купить {door.title} 🛒 в Москве по цене {door.price} руб. 🚪 Паритет двери 🛠️'
             door.h1 = door.title
             door.save()
+
+    def get_unique_slug(self, base_slug):
+        """
+        Функция для генерации уникального slug.
+        Если slug уже существует, добавляется суффикс с номером.
+        """
+        slug = base_slug
+        counter = 1
+        # Проверка на существование такого slug
+        while Door.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        return slug
